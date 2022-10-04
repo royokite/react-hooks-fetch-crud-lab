3e92c9c85682e32ba6ab986ec0bf947d448b2f4c
RoyOkite <royokite@gmail.com>
2022-10-05 00:30:59 +0300

    Completed assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, setQuestions, questions }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,38 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDelete(event) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE"
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        event.target.parentNode.remove()
+      })
+    
+  }
+
+  function handleChange(event) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({"correctIndex": Number(event.target.value)})
+    })
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,12 @@
 import React from "react";
+import QuestionItem from "./QuestionItem"
 
-function QuestionList() {
+function QuestionList({ questions, setQuestions }) {
+  const allQuestions = questions.map((question) => <QuestionItem key={question.id} question={question} setQuestions={setQuestions} questions={questions}/>)
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{allQuestions}</ul>
     </section>
   );
 }
